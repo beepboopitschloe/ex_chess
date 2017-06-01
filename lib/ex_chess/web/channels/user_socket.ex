131e8a9952d057692e04a2975dbd8b990ba00cb3
Noah Muth <noahmuth@gmail.com>
2017-06-01 11:59:43 +0200

    Set up for Heroku deployment

--- lib/ex_chess/web/channels/user_socket.ex
+++ lib/ex_chess/web/channels/user_socket.ex
@@ -5,8 +5,7 @@ defmodule ExChess.Web.UserSocket do
   channel "game:*", ExChess.Web.GameChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
